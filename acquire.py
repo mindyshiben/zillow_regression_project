532fc47643977d61a9a12145450583ae090acb5f
Mindy Shiben <mindy.shiben@gmail.com>
2022-06-09 16:56:28 -0500

    final_updates

--- acquire.py
+++ acquire.py
@@ -4,6 +4,13 @@ from env import get_db_url
 import os
 
 def get_zillow_data():
+
+    '''
+    This function acquires zillow data by accessing a SQL database and performing a SQL query to acquire
+    selected zillow tables and columns and return it to a dataframe. Additionally, data is stored in a .csv 
+    making it more efficient for future utilization of the same function.
+    '''
+
     filename = 'zillow.csv'
     
     if os.path.isfile(filename):
@@ -24,6 +31,13 @@ def get_zillow_data():
         return df 
 
 def get_zillow_locs():
+   
+    '''
+    This function acquires zillow data by accessing a SQL database and performing a SQL query to acquire
+    location information from the zillow database and return it to a dataframe. Additionally, data is stored in a .csv 
+    making it more efficient for future utilization of the same function.
+    '''
+
     filename = 'zillowloc.csv'
 
     if os.path.isfile(filename):
@@ -41,48 +55,3 @@ def get_zillow_locs():
         df.to_csv(filename)
 
         return df
-
-def get_zillow_zips():
-    filename = 'zips_df.csv'
-
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
-    
-    else:
-        sql = """
-        SELECT parcelid, latitude, longitude
-        FROM properties_2017
-        WHERE propertylandusetypeid = 261;
-        """
-
-        df = pd.read_sql(sql, get_db_url('zillow'))
-
-        df.to_csv(filename)
-
-        return df
-
-def get_zillow_other():
-    filename = 'zillow_other.csv'
-    
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
-    
-    else:
-        sql = """
-        SELECT parcelid
-        FROM properties_2017
-        JOIN predictions_2017 USING (parcelid)
-        WHERE transactiondate LIKE '2017%%'  and propertylandusetypeid = 261;
-        """
-
-        df = pd.read_sql(sql, get_db_url('zillow'))
-
-        df.to_csv(filename)
-
-        return df 
-
-
-
-
-
-
